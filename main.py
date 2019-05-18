3a94a0082e2e21b0394002d563a992fc48ae1d34
Seth <sethburster@gmail.com>
2019-05-18 14:05:18 -0400

    Imported Modules

---
+++ main.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import tensorflow as tg
+import numpy as np
+from sklearn.metrics import confusion_matrix
+
+from tensorflow.examples.tutorials.mnist import input_data
+data = input_data
